6c5c310209fa07fb02b4021bb11b1992c5397ded
Danilo Espino <daniloespino@gmail.com>
2018-05-01 18:35:29 -0300

    Se agrega el fútbol femenino.

--- js/change-fixture.js
+++ js/change-fixture.js
@@ -1,11 +1,14 @@
 // ID of the Google Spreadsheet
 var spreadsheetID = {
-		'primera':  "1EiRUYeVJDMstNGVABFXAqARRmwBbtlEkdQ5M0WWtJgE",
-		'segunda':  "1WpbU0TWSl_-UUJ_OsLywnadnq2UG2DRC8Te-Xnd3DX4",
-		'segundab': "1tXHJ_p2dIIIkytbmCadpP_ABdnyuN0mADAxRlrtoqeM"
+		'primera':   "1EiRUYeVJDMstNGVABFXAqARRmwBbtlEkdQ5M0WWtJgE",
+		'segunda':   "1WpbU0TWSl_-UUJ_OsLywnadnq2UG2DRC8Te-Xnd3DX4",
+		'segundab':  "1tXHJ_p2dIIIkytbmCadpP_ABdnyuN0mADAxRlrtoqeM",
+		'femeninoa': "1MvXQbIuk_hGiY1m3Dx-ANRhBRaUzg-QCu1nlzPTEeBs",
+		'femeninob': "1pue2yltn5w0Sw1OrWwhtEidDTJWKAsVoYsuT0Z_m3Yw"
 	};
 // Make sure it is public or set to Anyone with link can view 
 var url = "";
+var divisionalFem = "";
 
 function renderFixture(week, head="Torneo"){
 	if ((week>0) && (week<16)){
@@ -39,6 +42,24 @@ function renderFixtureSegunda(week){
 					$('#fixture').append('</td><tr><td align="center">'+entry[(week-1)*7+i].gsx$local.$t+'</td><td align="center">'+entry[(week-1)*7+i].gsx$resl.$t+'</td><td align="center">-</td><td align="center">'+entry[(week-1)*7+i].gsx$resv.$t+'</td><td align="center">'+entry[(week-1)*7+i].gsx$visita.$t+'</td></tr><tr><td align="center" colspan="5"><i>'+entry[(week-1)*7+i].gsx$specs.$t+'</i></td></tr>');
 	            } else {
 					$('#fixture').append('</td><tr style="background-color:#CBCBCB"><td align="center">'+entry[(week-1)*7+i].gsx$local.$t+'</td><td align="center">'+entry[(week-1)*7+i].gsx$resl.$t+'</td><td align="center">-</td><td align="center">'+entry[(week-1)*7+i].gsx$resv.$t+'</td><td align="center">'+entry[(week-1)*7+i].gsx$visita.$t+'</td></tr><tr><td style="background-color:#CBCBCB" align="center" colspan="5"><i>'+entry[(week-1)*7+i].gsx$specs.$t+'</i></td></tr>');
+	            }
+			}
+		});
+		fecha = week;	
+	}
+}
+
+function renderFixtureFemenino(week){
+	if ((week>0) && (week<10)){
+		$.getJSON(url, function(data) {
+			let entry = data.feed.entry;
+			$('#fixture-title').text("Femenino - Divisional "+ divisionalFem + " - Fecha "+week);
+			$('#fixture').text("");
+			for(let i=0; i<5; i++) {
+				if (Math.floor(i%2) == 0) {
+					$('#fixture').append('</td><tr><td align="center">'+entry[(week-1)*5+i].gsx$local.$t+'</td><td align="center">'+entry[(week-1)*5+i].gsx$resl.$t+'</td><td align="center">-</td><td align="center">'+entry[(week-1)*5+i].gsx$resv.$t+'</td><td align="center">'+entry[(week-1)*5+i].gsx$visita.$t+'</td></tr><tr><td align="center" colspan="5"><i>'+entry[(week-1)*5+i].gsx$specs.$t+'</i></td></tr>');
+	            } else {
+					$('#fixture').append('</td><tr style="background-color:#CBCBCB"><td align="center">'+entry[(week-1)*5+i].gsx$local.$t+'</td><td align="center">'+entry[(week-1)*5+i].gsx$resl.$t+'</td><td align="center">-</td><td align="center">'+entry[(week-1)*5+i].gsx$resv.$t+'</td><td align="center">'+entry[(week-1)*5+i].gsx$visita.$t+'</td></tr><tr><td style="background-color:#CBCBCB" align="center" colspan="5"><i>'+entry[(week-1)*5+i].gsx$specs.$t+'</i></td></tr>');
 	            }
 			}
 		});
